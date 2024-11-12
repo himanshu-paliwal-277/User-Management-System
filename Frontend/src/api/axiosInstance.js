723f9b3d5949ced433de73121eba922c3695649a
Himanshu Paliwal <hpaliwal364@gmail.com>
2024-11-12 14:59:50 +0530

    API_URL is updated

--- Frontend/src/api/axiosInstance.js
+++ Frontend/src/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-// const API_URL = import.meta.env.API_URL;
-const API_URL="http://localhost:3000/api/v1/users"
+const API_URL = import.meta.env.VITE_API_URL;
+// const API_URL = "http://localhost:3000/api/v1/users";
 
 console.log("API_URL = ", API_URL);
 const axiosInstance = axios.create(
